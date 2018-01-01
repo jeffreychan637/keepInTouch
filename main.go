12e9175dd3a9bd71843eecdbdbe2d5e8587d8fe8
Jeffrey Chan <jeffreychan637@gmail.com>
2017-12-31 23:01:56 -0800

    wrote all code to read data in from files and select random friend

--- main.go
+++ main.go
@@ -3,9 +3,19 @@ package main
 import (
     "bufio"
     "fmt"
+    "math/rand"
     "os"
     "strconv"
+    "time"
 )
+
+const contactedFileName = "contacted"
+const done = true
+
+var twilioAPIKey, phoneNumber, selectedFriend string
+var numFriends, selectedIndex int
+var friendsList []string
+var contactedMap map[int]bool = make(map[int]bool)
 
 func checkError(err error) {
     if (err != nil) {
@@ -24,47 +34,41 @@ func getNextLineText(scanner *bufio.Scanner) string {
     return scanner.Text()
 }
 
-func readConfigFile(configFileName string) {
-// return statement
-//(string, string, int, map[int]string, []string) {
-    configFile, err := os.Open(configFileName)
+func checkFileExistsOrCreate(fileName string) {
+    if _, err := os.Stat(fileName); os.IsNotExist(err) {
+        file, err := os.Create(fileName)
+        checkError(err)
+        file.Close()
+    }
+}
+
+func readFile(fileName string, readerFunc func(scanner *bufio.Scanner), channel chan bool) {
+    file, err := os.Open(fileName)
     checkError(err)
-    defer configFile.Close()
+    defer file.Close()
     
-    scanner := bufio.NewScanner(configFile)
-    skipLines(scanner, 1) // API comment
-    twilioAPIKey := getNextLineText(scanner)
-    
-    skipLines(scanner, 2) // New Line + Phone Number comment
-    phoneNumber := getNextLineText(scanner)
+    scanner := bufio.NewScanner(file)
+    readerFunc(scanner)
+    checkError(scanner.Err())
+    channel <- done
+}
+
+func readConfigFile(scanner *bufio.Scanner) {    
+    skipLines(scanner, 1) // Phone Number comment
+    phoneNumber = getNextLineText(scanner)
     
     skipLines(scanner, 2) // New Line + Friend Count comment
-    friendsCount := getNextLineText(scanner)
-    friendsCountInt, err := strconv.ParseInt(friendsCount, 0, 64)
+    numFriendsString := getNextLineText(scanner)
+    numFriendsInt64, err := strconv.ParseInt(numFriendsString, 0, 64)
     checkError(err)
+    numFriends = int(numFriendsInt64) // Assuming reasonable number of friends
     
     skipLines(scanner, 2) // New Line + Friend List comment
-    friendsList := getFriendsList(scanner, int(friendsCountInt)) // Assuming reasonable number of friends
+    friendsList = getFriendsList(scanner, numFriends)
     
-    fmt.Println(twilioAPIKey)
     fmt.Println(phoneNumber)
-    fmt.Println(friendsCount)
+    fmt.Println(numFriends)
     fmt.Println(friendsList)
-
-    checkError(scanner.Err())
-    
-    
-    
-    
-	fmt.Println(configFileName)
-// 	twilioApiKey := "API Key"
-//	phoneNumber := "123-456-7890"
-//	friendsList := [
-/* 		1 : "John Smith", 
-		2 : "Harry Potter",
-	]
-	alreadySelected := [1]
-	return twilioApiKey, phoneNumber, friendsList, alreadySelected */
 }
 
 func getFriendsList(scanner *bufio.Scanner, friendsCountInt int) []string {
@@ -78,8 +82,40 @@ func getFriendsList(scanner *bufio.Scanner, friendsCountInt int) []string {
     return friendsList
 }
 
+func readContactedFile(scanner *bufio.Scanner) {
+    for scanner.Scan() {
+        num, err := strconv.ParseInt(scanner.Text(), 0, 64)
+        checkError(err)
+        contactedMap[int(num)] = true
+       // contactedList = append(contactedList, int(num))  // Assuming reasonable number of friends
+    }
+}
+
+func processContactedFile(channel chan bool) {
+    checkFileExistsOrCreate(contactedFileName)
+    readFile(contactedFileName, readContactedFile, channel)
+}
+
+func readSecretFile(scanner *bufio.Scanner) {
+    skipLines(scanner, 1) // API comment
+    twilioAPIKey = getNextLineText(scanner)
+    
+    fmt.Println(twilioAPIKey)
+}
+
+//  This function selects a random friend that hasn't been contacted yet. 
+//  It chooses a random index and makes sure that that index is not in the
+//  contacted map. If it is, then it chooses another random index.
+//  This isn't the most efficient algorithm, but for the amount of
+//  friends we expect to be working with (< 200), this is more than fine.
 func selectRandomFriend() {
-	
+    random := rand.New(rand.NewSource(time.Now().UnixNano())) // use time as random seed
+    inMap := true
+	for inMap {
+	    selectedIndex = random.Intn(numFriends)
+	    inMap = contactedMap[selectedIndex]
+	}
+ 	selectedFriend = friendsList[selectedIndex]
 }
 
 func sendTwilioMessage() {
@@ -92,12 +128,26 @@ func cleanUpFile() {
 }
 
 func main() {
-    if len(os.Args) >= 2 {
-        configFileName := os.Args[1] // First arg (zero index) is path to file
+    if len(os.Args) >= 3 {
+        // First arg (zero index) is path to file
+        configFileName := os.Args[1]
+        secretFileName := os.Args[2]
         fmt.Println("Starting the script!")
-        readConfigFile(configFileName)
+        
+        channel := make(chan bool)
+        go processContactedFile(channel)
+        go readFile(configFileName, readConfigFile, channel)
+        go readFile(secretFileName, readSecretFile, channel)
+        for i := 0; i < 3; i++ { // wait till all 3 files have been read
+            <- channel
+        }
+        fmt.Println("Done processing all files")
+        
+        selectRandomFriend()
+        fmt.Printf("Today's friend is %s\n", selectedFriend)
+        
     } else {
-        fmt.Println("Please include the config file as the first command line argument to the script")
+        fmt.Println("Usage: keepInTouch config_file secret_file")
     }
 	
 }
